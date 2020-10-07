7d250f7aadab940154a374da36c4d243a7b04732
Nick Triller <NickTriller@gmail.com>
2020-10-07 20:14:41 +0200

    Implement plane

---
+++ engine/plane.go
@@ -0,0 +1,33 @@
+package engine
+
+import "math"
+
+type Plane struct {
+	Center   Point
+	Material Material
+	Normal Vec
+}
+
+const epsilon float64 = 0.00001
+
+// implement Hittable
+func (p *Plane) hit(ray *Ray, tMin, tMax float64) (*hitRecord, bool) {
+	denominator := Dot(p.Normal, ray.Direction)
+	// Protect from zero-division
+	if math.Abs(denominator) > epsilon {
+		t := Dot(p.Center.Subtract(ray.Origin), p.Normal) / denominator
+		if (t < tMax && t > tMin) && math.Abs(t) > epsilon {
+			hitPoint := ray.Interpolate(t)
+			record := &hitRecord{
+				p:         hitPoint,
+				t:         t,
+				material:  p.Material,
+			}
+			outwardNormal := p.Normal
+			record.setFaceNormal(ray, outwardNormal)
+			return record, true
+		}
+	}
+	return nil, false
+}
+
